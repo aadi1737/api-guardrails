1713de7bd0569395759bd490a4eb1ac54ee16fd7
Aaditya Prajapat <aadi1737ug@gmail.com>
2026-04-22 12:32:49 +0530

    addPost Controller added(*in last commit forgot to write controller code)

--- src/main/java/com/AadityaPrajapat/BackendAssignmentForAndazKumar/Controller/PostController.java
+++ src/main/java/com/AadityaPrajapat/BackendAssignmentForAndazKumar/Controller/PostController.java
@@ -1,6 +1,10 @@
 package com.AadityaPrajapat.BackendAssignmentForAndazKumar.Controller;
 
 import com.AadityaPrajapat.BackendAssignmentForAndazKumar.DTOs.PostRequestDTO;
+import com.AadityaPrajapat.BackendAssignmentForAndazKumar.DTOs.PostResponseDTO;
+import com.AadityaPrajapat.BackendAssignmentForAndazKumar.Service.PostService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostController {
 
-    @PostMapping("/api/posts")
-    public ResponseEntity<?> addPost(@RequestBody PostRequestDTO postRequestDTO){
+    @Autowired
+    private PostService postService;
 
+    @PostMapping("/api/posts")
+    public ResponseEntity<?> addPost(@Valid @RequestBody PostRequestDTO postRequestDTO){
+        //No need to check RequestBody cause @Valid
+
+        PostResponseDTO responseDTO = postService.addPost(postRequestDTO);
+        return ResponseEntity.ok(responseDTO);
     }
 }
