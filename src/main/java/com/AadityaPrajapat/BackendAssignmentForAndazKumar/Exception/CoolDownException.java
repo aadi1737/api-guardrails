22100a829c76bfe788efdf94f7efb384af3b0a01
Aaditya Prajapat <aadi1737ug@gmail.com>
2026-04-23 09:01:19 +0530

    GLobal Exception Handler added for CoolDownExcpetion(10 minutes)

---
+++ src/main/java/com/AadityaPrajapat/BackendAssignmentForAndazKumar/Exception/CoolDownException.java
@@ -0,0 +1,4 @@
+package com.AadityaPrajapat.BackendAssignmentForAndazKumar.Exception;
+
+public class CoolDownException {
+}
---
+++ src/main/java/com/AadityaPrajapat/BackendAssignmentForAndazKumar/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.AadityaPrajapat.BackendAssignmentForAndazKumar.Exception;
+
+public class GlobalExceptionHandler extends RuntimeException {
+  public GlobalExceptionHandler(String message) {
+    super(message);
+  }
+}
